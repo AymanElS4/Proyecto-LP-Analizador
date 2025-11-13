34a63f65fbe23fab8ec682695523f11740eef763
jorssanc <jorssanc@espol.edu.ec>
2025-11-12 22:28:00 -0500

    Analizador Léxico: Comentarios y Palabras Reservadas

---
+++ algoritmos/algoritmo_comentarios_y_palabrasReservadas.swift
@@ -0,0 +1,15 @@
+
+import Foundation
+while let var case break 
+func saludar(nombre: String) {
+    print("Hola \(nombre)") // imprimir mensaje
+}
+/*
+hola, este es un comentario multilinea
+*/
+// este es un comentario de una sola linea
+let mensaje = "Mundo"
+saludar(nombre: mensaje)
+
+
+// acá hay otro comentario
