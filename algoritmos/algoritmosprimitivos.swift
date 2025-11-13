6867599eb8e91cdb90c7b612d76ee14d01c22cb9
AymanElS4 <152307107+AymanElS4@users.noreply.github.com>
2025-11-12 20:06:39 -0500

    Lexer tipos primitivo y delimitadores. Creación de carpeta de algoritmos, logs y código

---
+++ algoritmos/algoritmosprimitivos.swift
@@ -0,0 +1,9 @@
+ 42
+    3.1415
+    true
+    false
+    "Hola Swift"
+    'A'
+    [1, 2, 3];
+    { "clave": "valor" }
+    (false)
---
+++ codigo/primitivos y limitadores.py
@@ -0,0 +1,103 @@
+import ply.lex as lex
+import datetime
+import os
+#tokens
+
+tokens = [
+    'INTEGER',
+    'FLOAT',
+    'BOOLEAN',
+    'STRING',
+    'CHARACTER',
+    'LPAREN', 'RPAREN',
+    'LBRACE', 'RBRACE',
+    'LBRACKET', 'RBRACKET',
+    'COMMA', 'SEMICOLON', 'COLON'
+]
+
+#Expresiones regulares en tokens
+t_LPAREN    = r'\('
+t_RPAREN    = r'\)'
+t_LBRACE    = r'\{'
+t_RBRACE    = r'\}'
+t_LBRACKET  = r'\['
+t_RBRACKET  = r'\]'
+t_COMMA     = r','
+t_SEMICOLON = r';'
+t_COLON     = r':'
+#Tipos primitivos
+def t_FLOAT(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+def t_INTEGER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_BOOLEAN(t):
+    r'(true|false)'
+    t.value = True if t.value == 'true' else False
+    return t
+
+def t_STRING(t):
+    r'\"([^\\\n]|(\\.))*?\"'
+    t.value = str(t.value)
+    return t
+
+def t_CHARACTER(t):
+    r'\'([^\\\n]|(\\.))\''
+    t.value = t.value[1:-1]  # eliminar comillas simples
+    return t
+
+
+# Ignorar espacios y tabulaciones
+t_ignore = ' \t'
+
+
+#manejo de errores
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print(f"[ERROR] Carácter ilegal: '{t.value[0]}' en línea {t.lexer.lineno}")
+    t.lexer.skip(1)
+
+def test_lexer(input_text, usuario_git="default"):
+    lexer = lex.lex()
+    lexer.input(input_text)
+
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+
+    fecha_hora = datetime.datetime.now().strftime("%d-%m-%Y-%Hh%M")
+    log_name =  "Proyecto-LP-Analizador\logs\lexico-" + usuario_git + "-" + fecha_hora + ".txt"
+
+    with open(log_name, "w", encoding="utf-8") as log:
+        log.write(f"LOG DE TOKENS ({fecha_hora})\n")
+        while True:
+            tok = lexer.token()
+            if not tok:
+                break
+            log.write(f"Línea {tok.lineno}: {tok.type} -> {tok.value}\n")
+
+    print(f"\nAnálisis léxico completado. Log guardado en: {log_name}")
+
+#ejemplo tipos primitivos y limitadores
+if __name__ == "__main__":
+    
+    codigo_swift = '''
+    42
+    3.1415
+    true
+    false
+    "Hola Swift"
+    'A'
+    [1, 2, 3];
+    { "clave": "valor" }
+    (false)
+    '''
+    test_lexer(codigo_swift, usuario_git="AymanElS4")
